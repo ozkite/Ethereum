a21ea976244e0a6ad838a3a8a9c2d8ac85ad2a62
☐𝕫𝕜 <oscarx22@gmail.com>
2025-08-27 00:20:45 +0800

    Update SimpleStorage.sol

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,1 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+/**
+ * @title MyStorage
+ * @dev A simple storage contract with events and access control
+ */
+contract MyStorage {
+    uint256 public number;
+    address public owner;
+
+    event NumberStored(address indexed sender, uint256 value);
+    event Upgraded(address indexed newImplementation);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    /**
+     * @dev Store a number (only owner)
+     * @param num Value to store
+     */
+    function store(uint256 num) public onlyOwner {
+        number = num;
+        emit NumberStored(msg.sender, num);
+    }
+
+    /**
+     * @dev Retrieve the current number
+     * @return The stored value
+     */
+    function retrieve() public view returns (uint256) {
+        return number;
+    }
+
+    /**
+     * @dev Upgrade to a new contract (for future use with proxies)
+     */
+    function upgrade() external onlyOwner {
+        emit Upgraded(address(this));
+    }
+}
